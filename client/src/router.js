d3b62251e1a2b585f1d112934a178bf7e94a45fb
Enryu <you@example.com>
2025-09-26 13:13:40 +0700

    hiep

--- client/src/router.js
+++ client/src/router.js
@@ -3,13 +3,16 @@ import Layout from './components/Layout.vue'
 import App from './components/App.vue'
 import LoginForm from './components/LoginForm.vue'
 import RegisterForm from './components/RegisterForm.vue'
+import EventReviewForm from './components/EventReviewForm.vue'
+import TicketPurchase from './components/TicketPurchase.vue'
+import TicketPayment from './components/TicketPayment.vue'
 
 const routes = [
   {
     path: '/',
     component: Layout,
     children: [
-      { path: '', name: 'Home', component: App },
+      { path: '', name: 'Home', component: TicketPayment },
       { path: '/login', name: 'Login', component: LoginForm },
       { path: '/register', name: 'Register', component: RegisterForm }
     ]
