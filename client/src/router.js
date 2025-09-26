ea823a70b65cc01216cbc0dfb7107e68ca645957
HoangViet158 <kirito15082004@gmail.com>
2025-09-26 13:06:15 +0700

    merge code

--- client/src/router.js
+++ client/src/router.js
@@ -1,24 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Layout from './components/Layout.vue'
-import App from './components/App.vue'
-import LoginForm from './components/LoginForm.vue'
-import RegisterForm from './components/RegisterForm.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Layout from "./components/Layout.vue";
+import App from "./components/App.vue";
+import LoginForm from "./components/LoginForm.vue";
+import RegisterForm from "./components/RegisterForm.vue";
+import EventDetail from "./components/user/EventDetail.vue";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: Layout,
     children: [
-      { path: '', name: 'Home', component: App },
-      { path: '/login', name: 'Login', component: LoginForm },
-      { path: '/register', name: 'Register', component: RegisterForm }
-    ]
-  }
-]
+      { path: "", name: "Home", component: App },
+      { path: "/login", name: "Login", component: LoginForm },
+      { path: "/register", name: "Register", component: RegisterForm },
+      { path: "/event-detail", name: "EventDetail", component: EventDetail },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
